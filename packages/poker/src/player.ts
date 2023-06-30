2919786d5ed19205a2846661ae3e404be971bab3
Tiste <tistee@outlook.com>
2023-06-30 20:08:10 +0700

    add magic fold test

--- packages/poker/src/player.ts
+++ packages/poker/src/player.ts
@@ -7,6 +7,7 @@ import {
 
 export class Player {
   public name: string;
+
   private handCards: CardType[];
 
   public boardCards: CardType[] = [];
--- packages/poker/src/tests/2.player.test.ts
+++ packages/poker/src/tests/2.player.test.ts
@@ -1,5 +1,5 @@
 import { Player } from "../player";
-import { makeReadableHandArray, shortNameToCard } from '../utils'
+import { makeReadableHandArray, shortNameToCard } from "../utils";
 import { EHandRank } from "../constants";
 
 describe("Test Player Class", function () {
@@ -22,34 +22,67 @@ describe("Test Player Class", function () {
     ]);
   });
 
+  it("player can be fold", function () {
+    const player = new Player({
+      name: "John",
+      hand: [shortNameToCard("2h"), shortNameToCard("3h")],
+      position: 1,
+    });
+    player.setFolded(true);
+    expect(player.isFolded()).toBe(true);
+
+    const player2 = new Player({
+      name: "Dendi",
+      hand: [shortNameToCard("AS"), shortNameToCard("AC")],
+      position: 2,
+    });
+    expect(player2.getHand()).toMatchObject([
+      shortNameToCard("AS"),
+      shortNameToCard("AC"),
+    ]);
+    player2.setFolded(true);
+    player2.setFolded(false); // can be unFolded
+    expect(player.isFolded()).toBe(true);
+  });
+
   // Bonus Test
-  it("should be compare all possible Hands and Flop", function() {
+  it("should be compare all possible Hands and Flop", function () {
     const player = new Player({
       name: "John",
       hand: [shortNameToCard("9h"), shortNameToCard("10h")],
-      position: 1
+      position: 1,
     });
     player.setBoardCards([
       shortNameToCard("2s"),
       shortNameToCard("3s"),
       shortNameToCard("4s"),
       shortNameToCard("5s"),
-      shortNameToCard("6s")
+      shortNameToCard("6s"),
     ]);
     const actual = player.allPossibleHands();
-    const allPossibleHands = actual.map(c => makeReadableHandArray(c));
+    const allPossibleHands = actual.map((c) => makeReadableHandArray(c));
     expect(allPossibleHands).toEqual([
-      "2s 3s 4s 9h 10h", "2s 3s 5s 9h 10h",
-      "2s 3s 6s 9h 10h", "2s 4s 5s 9h 10h",
-      "2s 4s 6s 9h 10h", "2s 5s 6s 9h 10h",
-      "3s 4s 5s 9h 10h", "3s 4s 6s 9h 10h",
-      "3s 5s 6s 9h 10h", "4s 5s 6s 9h 10h",
-      "2s 3s 4s 5s 9h", "2s 3s 4s 6s 9h",
-      "2s 3s 5s 6s 9h", "2s 4s 5s 6s 9h",
-      "3s 4s 5s 6s 9h", "2s 3s 4s 5s 10h",
-      "2s 3s 4s 6s 10h", "2s 3s 5s 6s 10h",
-      "2s 4s 5s 6s 10h", "3s 4s 5s 6s 10h",
-      "2s 3s 4s 5s 6s"
+      "2s 3s 4s 9h 10h",
+      "2s 3s 5s 9h 10h",
+      "2s 3s 6s 9h 10h",
+      "2s 4s 5s 9h 10h",
+      "2s 4s 6s 9h 10h",
+      "2s 5s 6s 9h 10h",
+      "3s 4s 5s 9h 10h",
+      "3s 4s 6s 9h 10h",
+      "3s 5s 6s 9h 10h",
+      "4s 5s 6s 9h 10h",
+      "2s 3s 4s 5s 9h",
+      "2s 3s 4s 6s 9h",
+      "2s 3s 5s 6s 9h",
+      "2s 4s 5s 6s 9h",
+      "3s 4s 5s 6s 9h",
+      "2s 3s 4s 5s 10h",
+      "2s 3s 4s 6s 10h",
+      "2s 3s 5s 6s 10h",
+      "2s 4s 5s 6s 10h",
+      "3s 4s 5s 6s 10h",
+      "2s 3s 4s 5s 6s",
     ]);
     expect(actual).not.toContainEqual(
       expect.arrayContaining([
@@ -57,7 +90,7 @@ describe("Test Player Class", function () {
         shortNameToCard("3s"),
         shortNameToCard("5s"),
         shortNameToCard("5c"),
-        shortNameToCard("6s")
+        shortNameToCard("6s"),
       ])
     );
   });
